326e023f964efb5a6cf7c037d52472215d48da47
Kumar <kumar0616@hotmail.com>
2017-08-19 13:15:29 -0700

    New Increment Program

---
+++ HelloWorld/src/sample/Increment.java
@@ -0,0 +1,15 @@
+package sample;
+
+public class Increment {
+	
+	public static void main(String args[]) {
+		int x=5;
+		System.out.println(x++);
+		int y=10;
+		System.out.println(++y);
+		int xy=10;
+		System.out.println(xy--);;
+		int yx=20;
+		System.out.println(--yx);
+	}
+}
